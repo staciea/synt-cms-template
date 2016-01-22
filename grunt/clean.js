6b78e10783b223b93d12a9269304006b813f8baa
Maxim Orlov <mail@orlovmax.com>
2016-01-23 01:25:34 +0200

    head-content block added to the base template, clean cache task fixed

--- grunt/clean.js
+++ grunt/clean.js
@@ -1,7 +1,10 @@
 // Clear files and folders
 module.exports = {
 	gitkeep: ['<%= theme.main %>/**/.gitkeep'],
-	cache: ['<%= theme.js %>/*.min.*.js',
-			'<%= theme.css %>/*.min.*.css',
-			'<%= theme.main %>/**/*.*.{png,jpg,gif,svg,ico}']
+	cache: [
+		'<%= theme.js %>/*.min.*.js',
+		'<%= theme.css %>/*.min.*.css',
+		'<%= theme.fonts %>/**/*.*.{eot,svg,ttf,otf,woff,woff2}',
+		'<%= theme.main %>/**/*.*.{png,jpg,gif,svg,ico}'
+	]
 }
